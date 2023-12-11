d5f79b74b2572dbee5bdc388b0ab5b9307e78899
Manjula Pradeep <manjulapradeep057@gmail.com>
2023-12-11 22:36:46 +0530

    About page update

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,9 @@ const About = () => {
         <div>
             <p>This application uses 'TheMealDB' public API.</p>
             <p>you can see delicious food recipes for free.</p>
+
+            <h4 className="liteLable">Credits : <a className="liteLable" target="blank" href="https://www.themealdb.com/">TheMealDB</a></h4>
+
         </div>
     )
 }
